fdc5cff48bd4dd447a609c2cb99a975b049e493d
DavidSanzMajolero <david.sanz.7e7@itb.cat>
2024-01-13 20:28:11 +0100

    Custome mode closes #9

--- ModeFunctions/Class1.cs
+++ ModeFunctions/Class1.cs
@@ -42,7 +42,103 @@ namespace ModeFunctions
             array[2] = (float)random.Next((int)min3, (int)max3 + 1);
             return array;
         }
-        
+        public static float[] CustomizeMode (float min0, float max0, float min1, float max1, float min2, float max2, float min3, float max3)
+        {            
+            float[] array = new float[4];
+            float[] arrayVoid = new float[0];
+            int count = 0, countGen = 0;
+            bool comprobation = false;
+            const string Error = "Els valors que has establert no hi són dins dels paràmetres";
+            GraphicFunc.Archer();
+            do
+            {
+                array[0] = Convert.ToInt32(Console.ReadLine());
+                if (array[0] < min0 || array[0] > max0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(Error);
+                    Console.ResetColor();
+                    count++;
+                }
+                else comprobation = true;
+                if (count == 3) 
+                {
+                    countGen++; 
+                    count = 0;
+                } 
+            } while (!comprobation && countGen < 3);
+            comprobation = false;
+            count = 0;
+            if (countGen < 3)
+            {
+                GraphicFunc.Barbarian();
+                do
+                {
+                    array[1] = Convert.ToInt32(Console.ReadLine());
+                    if (array[1] < min1 || array[1] > max1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(Error);
+                        Console.ResetColor();
+                        count++;
+                    }
+                    else comprobation = true;
+                    if (count == 3)
+                    {
+                        countGen++;
+                        count = 0;
+                    }
+                } while (!comprobation && countGen < 3);
+                comprobation = false;
+                count = 0;
+            }
+            if (countGen < 3)
+            {
+                GraphicFunc.Mage();
+                do
+                {
+                    array[2] = Convert.ToInt32(Console.ReadLine());
+                    if (array[2] < min2 || array[2] > max2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(Error);
+                        Console.ResetColor();
+                        count++;
+                    }
+                    else comprobation = true;
+                    if (count == 3)
+                    {
+                        countGen++;
+                        count = 0;
+                    }
+                } while (!comprobation && countGen < 3);
+                comprobation = false;
+                count = 0;
+            }
+            if (countGen < 3)
+            {
+                GraphicFunc.Druid();
+                do
+                {
+                    array[3] = Convert.ToInt32(Console.ReadLine());
+                    if (array[3] < min3 || array[3] > max3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(Error);
+                        Console.ResetColor();
+                        count++;
+                    }
+                    else comprobation = true;
+                    if (count == 3)
+                    {
+                        countGen++;
+                        count = 0;
+                    }
+                } while (!comprobation && countGen < 3);
+            }
+            if (countGen < 3) return array;
+            else return arrayVoid;
+        }
     }
 }
 //defArquera = 1 - (defArquera / 100); //defensa del monstre en valor percentual
